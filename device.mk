44f68418cc072c082e8bd042c71a5fa5a38ca1f9
wayneeg <waynee@outlook.com.br>
2020-12-01 09:11:19 -0300

    use GcamGo default
    Signed-off-by: wayneeg <waynee@outlook.com.br>

--- device.mk
+++ device.mk
@@ -141,7 +141,7 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-service \
     android.hardware.camera.provider@2.5 \
     vendor.qti.hardware.camera.device@1.0 \
-    Snap
+    GCamGo
 
 # Codec2 modules
 PRODUCT_PACKAGES += \
