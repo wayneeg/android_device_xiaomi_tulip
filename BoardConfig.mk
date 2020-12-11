0b3ec116881ede65511bf1c2ab2f3c6309bd5787
wayneeg <waynee@outlook.com.br>
2020-12-11 14:37:34 -0300

    SEPolicy.mk --> sepolicy.mk
    Signed-off-by: wayneeg <waynee@outlook.com.br>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -258,7 +258,7 @@ TARGET_PROVIDES_QTI_TELEPHONY_JAR := true
 VENDOR_SECURITY_PATCH := 2020-04-05
 
 # SELinux
-include device/qcom/sepolicy-legacy-um/SEPolicy.mk
+include device/qcom/sepolicy-legacy-um/sepolicy.mk
 SELINUX_IGNORE_NEVERALLOWS := true
 BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
 BOARD_PLAT_PUBLIC_SEPOLICY_DIR += $(DEVICE_PATH)/sepolicy/public
