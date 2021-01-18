d1efc140e0e2a27165ecd277dee9bee1c7efb576
jefinhodatnt <jefiinhorelikia@gmail.com>
2021-01-18 14:51:22 -0300

    tulip: Fullscreen/Immersive Gestures
    removes space between the keyboard and the edge
    BIG thanks for @DanGLES3 for module
    
    Author: jefinhodatnt <jefiinhorelikia@gmail.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -4,3 +4,7 @@ export TEMPORARY_DISABLE_PATH_RESTRICTIONS=true
 export USE_CCACHE=1
 export CCACHE_EXEC=$(command -v ccache)
 rm -rf vendor/qcom/opensource/power
+rm -rf frameworks/base/package/overlays/NavigationBarModeGesturalOverlay
+rm -rf frameworks/base/package/overlays/NavigationBarModeGesturalOverlayWideBack
+rm -rf frameworks/base/package/overlays/NavigationBarModeGesturalOverlayNarrowBack
+rm -rf frameworks/base/package/overlays/NavigationBarModeGesturalOverlayExtraWideBack
