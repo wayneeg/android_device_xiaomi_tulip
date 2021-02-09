MrPurple666/android_device_xiaomi_tulip-r

Tulip: Fullscreen/Immersive Gestures
removes space between the keyboard and the edge
BIG thanks for @DanGLES3 for module

Author: jefinhodatnt <jefiinhorelikia@gmail.com>

vendorsetup.sh
@@ -1,0 +1,4 @@
+rm -rf frameworks/base/package/overlays/NavigationBarModeGesturalOverlay
+rm -rf frameworks/base/package/overlays/NavigationBarModeGesturalOverlayWideBack
+rm -rf frameworks/base/package/overlays/NavigationBarModeGesturalOverlayNarrowBack
+rm -rf frameworks/base/package/overlays/NavigationBarModeGesturalOverlayExtraWideBack

wayneeg/android_device_xiaomi_tulip

vendorsetup.sh
@@ -1,0 +1,4 @@
+rm -rf frameworks/base/packages/overlays/NavigationBarModeGesturalOverlay
+rm -rf frameworks/base/packages/overlays/NavigationBarModeGesturalOverlayWideBack
+rm -rf frameworks/base/packages/overlays/NavigationBarModeGesturalOverlayNarrowBack
+rm -rf frameworks/base/packages/overlays/NavigationBarModeGesturalOverlayExtraWideBack