bdede0ef03ae79cc80c3510436210280b7028fbf
wayneeg <waynee@outlook.com.br>
2021-02-09 14:14:24 -0300

    tulip: update FP to february
    * update Google Redfin FP to february
    * update PRIVATE_BUILD_DESC to Redfin
    
    Signed-off-by: wayneeg <waynee@outlook.com.br>

--- derp_tulip.mk
+++ derp_tulip.mk
@@ -45,9 +45,8 @@ PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 TARGET_VENDOR_PRODUCT_NAME := tulip
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="redfin-user 11 RQ1A.210105.003 7005429 release-keys"
+    PRIVATE_BUILD_DESC="redfin-user 11 RQ1A.210205.004 7038034 release-keys"
 
-#Build FP to be picked by both system and vendor
-BUILD_FINGERPRINT := "google/redfin/redfin:11/RQ1A.210105.003/7005429:user/release-keys"    
+BUILD_FINGERPRINT := "google/redfin/redfin:11/RQ1A.210205.004/7038034:user/release-keys"   
 
 
