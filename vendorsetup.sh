67fe3e2bf4c57fce3324f955c74869414b2dadf4
wayneeg <waynee@outlook.com.br>
2021-03-03 09:11:41 -0300

    tulip: rebase vendorsetup
    Signed-off-by: wayneeg <waynee@outlook.com.br>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,3 +1,8 @@
+export SKIP_ABI_CHECKS=true
+export SELINUX_IGNORE_NEVERALLOWS=true
+export TEMPORARY_DISABLE_PATH_RESTRICTIONS=true
+export USE_CCACHE=1
+export CCACHE_EXEC=$(command -v ccache)
 rm -rf frameworks/base/packages/overlays/NavigationBarModeGesturalOverlay
 rm -rf frameworks/base/packages/overlays/NavigationBarModeGesturalOverlayWideBack
 rm -rf frameworks/base/packages/overlays/NavigationBarModeGesturalOverlayNarrowBack
